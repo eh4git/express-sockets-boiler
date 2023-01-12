d0208675f938fa68dd7e081412f98f3b898069fb
eh4git <ehirsch760@gmail.com>
2023-01-11 21:19:18 -0800

    seperated chat rooms, moderated and unfiltered chats, profanity filter on moderated chat

---
+++ sockets-controller.js
@@ -0,0 +1,39 @@
+const { moderateText } = require("./openAI");
+const { filterObscenities, hasObscenities } = require("./purgoMalum");
+
+// Server side socket event handling
+function socketsController(socket) {
+  console.log("A user connected");
+
+  socket.on("join-chat", function (...args) {
+    socket.join("chat");
+    socket.to("chat").emit("join-chat", args[0]);
+  });
+
+  socket.on("new-chat-message", async function (name, text) {
+    console.log(name, text);
+    const textObj = { moderated: false, text };
+    socket.to("chat").emit("new-chat-message", name, textObj);
+  });
+
+  socket.on("join-moderated", function (...args) {
+    socket.join("moderated");
+    socket.to("moderated").emit("join-moderated", args[0]);
+  });
+
+  socket.on("new-moderated-message", async function (name, text) {
+    console.log(name, text);
+    const filteredText = await moderateText(
+      name,
+      await filterObscenities(text)
+    );
+    if (filteredText.moderated)
+      socket.to("moderated").emit("message-blocked", filteredText);
+    socket.to("moderated").emit("new-moderated-message", name, filteredText);
+  });
+
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
+  });
+}
+module.exports = socketsController;
